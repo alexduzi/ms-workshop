5c40a7f1aede6ffafea4ab35c411631896079bae
Alex Duzi <alex.henrique.duzi>
2024-10-09 21:43:57 -0300

    removing rest template implementation and adding open feign

--- hr-payroll/src/main/java/com/alexduzi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/alexduzi/hrpayroll/services/PaymentService.java
@@ -1,32 +1,21 @@
 package com.alexduzi.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.alexduzi.hrpayroll.entities.Payment;
 import com.alexduzi.hrpayroll.entities.Worker;
+import com.alexduzi.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, int days) {
 		
-		Map<String, String> params = new HashMap<>();
-		
-		params.put("id", workerId.toString());
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
