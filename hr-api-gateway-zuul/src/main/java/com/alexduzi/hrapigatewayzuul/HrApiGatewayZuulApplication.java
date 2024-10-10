9ffb72a888af0e4b292ae89916af6dabfaaec3c1
Alex Duzi <alex.henrique.duzi>
2024-10-10 20:32:05 -0300

    configuring zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/alexduzi/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/alexduzi/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,12 @@ package com.alexduzi.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+// habilita este serviço como um client do eureka e como um gateway proxy
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
