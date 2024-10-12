1584f8c7c023d77d70c0c49193a8fbeb1828f710
Alex Duzi <alex.henrique.duzi>
2024-10-12 14:04:52 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/alexduzi/hruser/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.alexduzi.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexduzi.hruser.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    User findByEmail(String email);
+}
